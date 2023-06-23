c64e53216d1b6fdb115e0b65077895d24101f5d7
Pranay Lamse <99016480+pranaylamsecode@users.noreply.github.com>
2023-06-23 17:15:09 +0530

    update to react

---
+++ resources/js/components/Example.jsx
@@ -0,0 +1,40 @@
+
+import ReactDOM from 'react-dom/client';
+import React, { useState } from "react";
+import apiConfig from "axios";
+function Example() {
+
+    const [file, setFile] = useState(null);
+    const formData = new FormData();
+
+    const onFileChange = (e) => setFile(e.target.files[0]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        formData.append("file", file);
+        console.log(file);
+        apiConfig.post("/upload-file", formData);
+        // alert("File is Uploaded.");
+    };
+    return (
+        <div>
+            <h1>TestComponent</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="file" name="file" onChange={onFileChange} />
+                <button>Upload</button>
+            </form>
+        </div>
+    );
+}
+
+export default Example;
+
+if (document.getElementById('root')) {
+    const Index = ReactDOM.createRoot(document.getElementById("root"));
+
+    Index.render(
+        <React.StrictMode>
+            <Example/>
+        </React.StrictMode>
+    )
+}
